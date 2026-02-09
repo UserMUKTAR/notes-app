7d5ada6efe7c105bb752da7686f23bebbeef3d74
Muktar Islam <muktarmerch83@gmail.com>
2026-02-10 01:16:37 +0530

    Initial React frontend

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Notes from "./Notes";
+
+function App() {
+  return <Notes />;
+}
+
+export default App;
+
---
+++ src/noteApi.js
@@ -0,0 +1,11 @@
+
+
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/notes";
+
+export const getNotes = () => axios.get(API_URL);
+
+export const createNote = (note) => axios.post(API_URL, note);
+
+export const deleteNote = (id) => axios.delete(`${API_URL}/${id}`);
